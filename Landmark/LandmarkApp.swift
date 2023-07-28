81dc8f2867ce55681b758e227a1b879f1973f42c
Louis Mille <mille.louis1@gmail.com>
2023-07-28 21:03:15 +0200

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Louis Mille on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
